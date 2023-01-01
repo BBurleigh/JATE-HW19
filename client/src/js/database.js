1259ce4d6e51652a46b30e4609ad9cbc5f69e5e6
Brayden <brayburl224@gmail.com>
2023-01-01 15:29:26 -0500

    added more to putDb console.log and drafted out getDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
 
   const jateDb = await('jate', 1);
@@ -25,11 +24,27 @@ export const putDb = async (content) => {
 
   const result = await request;
 
-  console.log(result);
-  
+  console.log('The following information has been saved to the database', result);
+
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  const jateDb = await('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log('result.value', result);
+
+  return result;
+
+};
 
 initdb();
